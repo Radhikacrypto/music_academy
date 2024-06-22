4a99f8464a2df0ae5a839fbee91141b8dfa5e550
Radhika Bacharwar <radhikabacharwar20@gmail.com>
2024-06-23 00:29:54 +0530

    hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,5 +1,8 @@
-
-import Link from "next/link"
+"use client";
+import React from "react";
+import { Button } from "./ui/moving-border";
+import Link from "next/link";
+import { Spotlight } from "./ui/Spotlight";
 function HeroSection() {
     
 
@@ -7,6 +10,10 @@ function HeroSection() {
         <div
         className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0"
         >
+            <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="violet"
+      />
          <div className="p-4 relative z-10 w-full text-center">
          <h1
             className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400"
@@ -17,7 +24,12 @@ function HeroSection() {
             <div className="mt-4">
                 <Link href={"/courses"}>
                    
-                    Explore Courses 
+                <Button
+        borderRadius="1.75rem"
+        className="bg-white dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800"
+      >
+        Explore Courses
+      </Button>
                    
                 </Link>
             </div>
